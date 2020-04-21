49762497301038e8f92579e33fbe9c39c5d43e5e
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-21 14:53:15 +0100

    Add limit constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
-
+  LIMIT = 90
+  
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    @balance += money 
+    @balance += money
   end
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,12 @@ describe Oystercard do
     subject.top_up(20)
     expect(subject.balance).to eq 20
   end
+
+  it 'increase balance (using change rspec syntax)' do
+    expect {subject.top_up 20}.to change{subject.balance}.by 20
+  end
+
+  it 'limit instance variable returns 90' do
+    expect(Oystercard::LIMIT).to eq 90
+  end
 end
