50bdd6132ed0747154faa1284441c49d5721ecda
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-21 08:14:28 +0100

    Add balance method with default balance

---
+++ lib/oystercard.rb
@@ -0,0 +1,11 @@
+class Oystercard
+  #attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def balance
+    @balance
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  it 'Balance method returns 0 when initialized' do
+    expect(subject.balance).to eq 0
+  end
 end
